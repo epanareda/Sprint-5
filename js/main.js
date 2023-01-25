85bde1dbb70f68d39beb61d2c98a4891fef91210
epanareda <epanareda@gmail.com>
2023-01-25 19:20:42 +0100

    exercici 4

--- js/main.js
+++ js/main.js
@@ -2,10 +2,44 @@
 const next_btn = document.querySelector("#next-joke-btn");
 const joke_text = document.querySelectorAll("p")[1];
 const punct_btn_conatiner = document.querySelector("#punct-joke-container");
+const weather_text = document.querySelector("p");
 
 // API variables.
+// Father jokes.
 const api_url = "https://icanhazdadjoke.com/";
 const api_headers = {"Accept": "application/json"};
+// Weather.
+const api_weather_url = "https://api.open-meteo.com/v1/forecast?latitude=41.39&longitude=2.16&current_weather=true"; // Weather in Barcelona.
+const weather_code = {
+    0: "clear sky",
+    1: "mainly clear",
+    2: "partly cloudy",
+    3: "overcast",
+    45: "fog",
+    48: "depositing rime fog",
+    51: "drizzle light",
+    53: "drizzle moderate",
+    55: "drizzle dense intensity",
+    56: "freezing drizzle light",
+    57: "freezing dizzle heavy intensity",
+    61: "rain slight",
+    63: "rain moderate",
+    65: "rain heavy intensity",
+    66: "freezing rain light",
+    67: "freezing rain heavy intensity",
+    71: "snow fall slight",
+    73: "snow fall moderate",
+    75: "snow fall heavy intensity",
+    77: "snow grains",
+    80: "rain shower slight",
+    81: "rain shower moderate",
+    82: "rain shower violent",
+    85: "snow showers slight",
+    86: "snow showers heavy",
+    95: "thunderstorm",
+    96: "thunderstorm with slight hail",
+    99: "thunderstorm with heavy hail"
+};
 
 // Data storage variables.
 const reportAcudits = [];
@@ -19,9 +53,15 @@ next_btn.addEventListener("click", () => {
             joke_text.textContent = `" ${json.joke} "`;
             // The new joke shown is added the the report, it's score, in case it's voted, will be assign later on.
             add_report(json.joke);
-        })
+        });
         add_punct_btn();
 });
+
+// Here the weather api is acceced by fetch.
+fetch(api_weather_url)
+    .then(response => response.json())
+    // .then(json => console.log(weather_code[json.current_weather.weathercode]))
+    .then(json => weather_text.textContent = `Avui: ${weather_code[json.current_weather.weathercode]} | ${json.current_weather.temperature}ºC`);
 
 // This function adds the buttons to vote the jokes once they are shown on screen, with all of it's styles and functionalities.
 function add_punct_btn() {
